526814e710efbf198eeb9c72a2fcd44ca6e4883a
Elril0000 <Elril0000@gmail.com>
2013-07-06 15:00:27 -0400

    Finalized version 1.0BETA Official release to come soon

--- src/MainWin.cpp
+++ src/MainWin.cpp
@@ -6,13 +6,32 @@ MainWin::MainWin(): QMainWindow()
 	_refreshButton = new QPushButton();
 	_lockBoolButton = new QPushButton();
 	_lockBoolButton->setCheckable(true);
+	_lockBoolButton->setChecked(true);
 	_addTabButton = new QPushButton;
 	
+	_refreshButton->setIcon(QIcon::fromTheme("view-refresh"));
+	_lockBoolButton->setIcon(QIcon::fromTheme("object-locked"));
+	_addTabButton->setIcon(QIcon::fromTheme("add"));
+	//_lockBoolButton->setFlat(true);
+	//_lockBoolButton->setUpdatesEnabled(false);
+	
+	
 	_tab = new QTabWidget;
-	_tab->addTab(newTab(tr("http://candies.aniwey.net/index.php?pass=wtrbr")), tr("(New Game)"));
+	_tab->addTab(newTab(tr("http://candies.aniwey.net/index.php")), tr("(New Game)"));
+	_tab->setTabsClosable(true);
 	_passwordLabel = new QLabel;
 	_passwordLine = new QLineEdit;
+	_passwordLine->setReadOnly(true);
 	
+	connect(_passwordLine, SIGNAL(returnPressed()), this, SLOT(loadPassword()));
+	connect(_enterButton, SIGNAL(clicked()), this, SLOT(loadPassword()));
+	connect(_refreshButton, SIGNAL(clicked()), this, SLOT(refresh()));
+	connect(_lockBoolButton, SIGNAL(clicked(bool)), this, SLOT(lockPassword()));
+	connect(_addTabButton, SIGNAL(clicked()), this, SLOT(newTabSlots()));
+	connect(_tab, SIGNAL(currentChanged(int)), this, SLOT(changeTab()));
+	connect(_tab->tabBar(), SIGNAL(tabCloseRequested(int)), this, SLOT(closeTab(int)));
+	
+	_tab->setCornerWidget(_addTabButton);
 	QHBoxLayout *bottomLayout = new QHBoxLayout;
 	
 	bottomLayout->addWidget(_lockBoolButton);
@@ -20,6 +39,7 @@ MainWin::MainWin(): QMainWindow()
 	bottomLayout->addWidget(_passwordLine);
 	bottomLayout->addWidget(_enterButton);
 	bottomLayout->addWidget(_refreshButton);
+	bottomLayout->setAlignment(Qt::AlignRight);
 	
 	QVBoxLayout *layout = new QVBoxLayout;
 	
@@ -30,6 +50,11 @@ MainWin::MainWin(): QMainWindow()
 	widgetCountainer->setLayout(layout);
 	
 	setCentralWidget(widgetCountainer);
+	showMaximized();
+	QString windowTitles("Candy Box GUI v");
+	windowTitles += VERSION;
+	setWindowTitle(windowTitles);
+	setWindowIcon(QIcon("image/candybox.png"));
 }
 
 QWebView *MainWin::ActualPage()
@@ -51,4 +76,76 @@ QWidget *MainWin::newTab(QUrl url)
 	webPage->load(url);
 	
 	return widgetCountainer;
+}
+
+void MainWin::loadPassword()
+{
+	if(!_passwordLine->text().isEmpty())
+	{
+		if(QMessageBox::question(this, tr("Unsaved Change"), tr("All unsaved change will be lost.\nContinue?"), QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
+		{
+			ActualPage()->load(QUrl("http://candies.aniwey.net/index.php?pass=" + _passwordLine->text()));
+			_tab->setTabText(_tab->currentIndex(), _passwordLine->text());
+		}
+	}
+	
+	else
+	{
+		if(QMessageBox::question(this, tr("Unsaved Change"), tr("All unsaved change will be lost.\nContinue?"), QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
+		{
+			ActualPage()->load(QUrl("http://candies.aniwey.net/index.php"));
+			_tab->setTabText(_tab->currentIndex(), tr("(New Game)"));
+		}
+	}
+}
+
+void MainWin::lockPassword()
+{
+	if(_lockBoolButton->isChecked())
+	{
+		_lockBoolButton->setIcon(QIcon::fromTheme("object-locked"));
+		_passwordLine->setReadOnly(true);
+	}
+	else
+	{
+		_lockBoolButton->setIcon(QIcon::fromTheme("object-unlocked"));
+		_passwordLine->setReadOnly(false);
+	}
+}
+
+void MainWin::newTabSlots()
+{
+	_tab->addTab(newTab(tr("http://candies.aniwey.net/index.php")), tr("(New Game)"));
+}
+
+void MainWin::refresh()
+{
+	if(QMessageBox::question(this, tr("Unsaved Change"), tr("All unsaved change will be lost.\nContinue?"), QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
+		ActualPage()->reload();
+}
+
+void MainWin::changeTab()
+{
+	if(_tab->tabText(_tab->currentIndex()) == tr("(New Game)"))
+	{
+		_passwordLine->setText("");
+	}
+	else
+	{
+		_passwordLine->setText(_tab->tabText(_tab->currentIndex()));
+	}
+}
+
+void MainWin::closeTab(int index)
+{
+	if(_tab->count() <= 1)
+	{
+		if(QMessageBox::question(this, tr("Unsaved Change"), tr("All unsaved change will be lost.\nContinue?"), QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
+			qApp->quit();
+	}
+	else
+	{
+		if(QMessageBox::question(this, tr("Unsaved Change"), tr("All unsaved change will be lost.\nContinue?"), QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
+			_tab->removeTab(index);
+	}
 }
--- src/MainWin.hpp
+++ src/MainWin.hpp
@@ -1,13 +1,23 @@
 #include <QtWidgets>
 #include <QtWebKit>
 #include <QtWebKitWidgets>
+#include "Config.hpp"
 
 class MainWin : public QMainWindow
 {
+	Q_OBJECT
 public:
     MainWin();
 	QWebView *ActualPage();
 	QWidget *newTab(QUrl url);
+	
+public slots:
+	void loadPassword();
+	void lockPassword();
+	void newTabSlots();
+	void refresh();
+	void changeTab();
+	void closeTab(int index);
 	
 private:
 	QPushButton *_enterButton;
